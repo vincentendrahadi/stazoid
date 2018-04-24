ebf1d20f6c17017244134f8a317880c728f973f7
Gisela <giselasupardi@gmail.com>
2018-04-24 22:13:14 +0700

    Implement pause button

---
+++ Assets/Scripts/PlaySceneController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlaySceneController : MonoBehaviour {
+	[SerializeField]
+	private Canvas pauseCanvas;
+
+	[SerializeField]
+	private AudioSource bgmSource;
+	[SerializeField]
+	private AudioSource[] sfxSource;
+
+	[SerializeField]
+	private Toggle bgmToggle;
+	[SerializeField]
+	private Toggle sfxToggle;
+
+	void Start() {
+		pauseCanvas.gameObject.SetActive (false);
+	}
+
+	public void showPauseCanvas() {
+		pauseCanvas.gameObject.SetActive (true);
+	}
+
+	public void hidePauseCanvas() {
+		pauseCanvas.gameObject.SetActive (false);
+	}
+
+	public void toggleBGM() {
+		if (bgmToggle.isOn) {
+			bgmSource.volume = 1f;
+		} else {
+			bgmSource.volume = 0f;
+		}
+	}
+
+	public void toggleSFX() {
+		if (sfxToggle.isOn) {
+			sfxSource[0].volume = 0.3f;
+			sfxSource[1].volume = 1f;
+		} else {
+			sfxSource[0].volume = 0f;
+			sfxSource[1].volume = 0f;
+		}
+	}
+}
+
