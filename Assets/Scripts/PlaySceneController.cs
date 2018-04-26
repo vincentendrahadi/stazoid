35957d271977c3ffac1f5b15f5dc444e0e5145cc
Vincent Endrahadi <vincentendrahadi01@gmail.com>
2018-04-27 03:59:57 +0700

    Added player pref for volume settings

--- Assets/Scripts/PlaySceneController.cs
+++ Assets/Scripts/PlaySceneController.cs
@@ -15,7 +15,7 @@ public class PlaySceneController : MonoBehaviour {
 	private AudioSource[] sfxSource;
 
 	[SerializeField]
-	private Toggle bgmToggle;
+	private Toggle muteToggle;
 	[SerializeField]
 	private Toggle sfxToggle;
 
@@ -24,6 +24,34 @@ public class PlaySceneController : MonoBehaviour {
 
 	[SerializeField]
 	private Slider sfxSlider;
+
+	private static bool isMute;
+
+	void Awake() {
+		Debug.Log("sfx" + PlayerPrefs.GetFloat("sfxVolume"));
+		bgmSlider.value = PlayerPrefs.GetFloat("bgmVolume");
+		sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume");
+		Debug.Log ("Setes");
+		if (PlayerPrefs.GetInt("isMute") == 1) {
+			muteToggle.isOn = true;
+			bgmSource.volume = 0f;
+			sfxSource [0].volume = 0f;
+			sfxSource [1].volume = 0f;
+		} else {
+			muteToggle.isOn = false;
+			bgmSource.volume =  bgmSlider.value;
+			sfxSource [0].volume = sfxSlider.value;
+			sfxSource [1].volume = sfxSlider.value;
+		}
+
+	}
+
+	public void saveState() {
+		PlayerPrefs.SetInt ("isMute",isMute == true ? 1 : 0);
+		PlayerPrefs.SetFloat("bgmVolume", bgmSlider.value);
+		PlayerPrefs.SetFloat("sfxVolume", sfxSlider.value);
+		Debug.Log("sfx set " +  sfxSlider.value);
+	}
 
 	public void showPauseCanvas() {
 		pauseCanvas.gameObject.SetActive (true);
@@ -34,25 +62,33 @@ public class PlaySceneController : MonoBehaviour {
 	}
 
 	public void toggleMute() {
-		if (bgmSource.volume > 0) {
+		if (muteToggle.isOn) {
 			bgmSource.volume = 0f;
 			sfxSource [0].volume = 0f;
 			sfxSource [1].volume = 0f;
+			isMute = true;
 		} else {
 			bgmSource.volume =  bgmSlider.value;
 			sfxSource [0].volume = sfxSlider.value;
 			sfxSource [1].volume = sfxSlider.value;
+			isMute = false;
 		}
-
+		saveState ();
 	}
 
 	public void bgmVolume() {
-		bgmSource.volume = bgmSlider.value;
+		if (!isMute) {
+			bgmSource.volume = bgmSlider.value;	
+		}
+		saveState ();
 	}
 
 	public void sfxVolume() {
-		sfxSource [0].volume = sfxSlider.value;
-		sfxSource [1].volume = sfxSlider.value;
+		if (!isMute) {
+			sfxSource [0].volume = sfxSlider.value;
+			sfxSource [1].volume = sfxSlider.value;
+		}
+		saveState ();
 	}
 }
 
