dbd58a3d9c95e73473eed5a7e4f3c035a7317669
Vincent Endrahadi <vincentendrahadi01@gmail.com>
2018-04-26 16:43:23 +0700

    Fixed toggle mute button and add volume handler in single player controller

--- Assets/Scripts/PlaySceneController.cs
+++ Assets/Scripts/PlaySceneController.cs
@@ -19,6 +19,12 @@ public class PlaySceneController : MonoBehaviour {
 	[SerializeField]
 	private Toggle sfxToggle;
 
+	[SerializeField]
+	private Slider bgmSlider;
+
+	[SerializeField]
+	private Slider sfxSlider;
+
 	public void showPauseCanvas() {
 		pauseCanvas.gameObject.SetActive (true);
 	}
@@ -27,15 +33,26 @@ public class PlaySceneController : MonoBehaviour {
 		pauseCanvas.gameObject.SetActive (false);
 	}
 
-	public void mute() {
-		bgmSource.volume = 0f;
-		sfxSource [0].volume = 0f;
-		sfxSource [1].volume = 0f;
+	public void toggleMute() {
+		if (bgmSource.volume > 0) {
+			bgmSource.volume = 0f;
+			sfxSource [0].volume = 0f;
+			sfxSource [1].volume = 0f;
+		} else {
+			bgmSource.volume =  bgmSlider.value;
+			sfxSource [0].volume = sfxSlider.value;
+			sfxSource [1].volume = sfxSlider.value;
+		}
+
 	}
 
-	public void unmute() {
-		// isi sesuai nilai slider * MAX masing-masing (SoundInfo.MAX_something)
+	public void bgmVolume() {
+		bgmSource.volume = bgmSlider.value;
 	}
-		
+
+	public void sfxVolume() {
+		sfxSource [0].volume = sfxSlider.value;
+		sfxSource [1].volume = sfxSlider.value;
+	}
 }
 
